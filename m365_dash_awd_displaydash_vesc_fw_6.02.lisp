7a18892ea906b2d1a3379abeb3f458c303b75589
PierreDisplayDash <contact@displaydash.eu>
2024-05-29 14:31:56 +0200

    Update m365_dash_awd_displaydash_vesc_fw_6.02.lisp
    try 200 DD flag into get-batt

--- m365_dash_awd_displaydash_vesc_fw_6.02.lisp
+++ m365_dash_awd_displaydash_vesc_fw_6.02.lisp
@@ -1,4 +1,4 @@
-; test 3
+; test 4
 ; M365 dashboard compability lisp script v0.6 by Netzpfuscher and 1zuna
 ; UART Wiring: red=5V black=GND yellow=COM-TX (UART-HDX) green=COM-RX (button)+3.3V with 1K Resistor
 ; Guide (German): https://rollerplausch.com/threads/vesc-controller-einbau-1s-pro2-g30.6032/
@@ -162,8 +162,9 @@
         )
         
         ; batt field
-        (bufset-u8 tx-frame 7 (*(get-batt) 100))
-
+        ; (bufset-u8 tx-frame 7 (*(get-batt) 100))
+        (bufset-u8 tx-frame 7 200))
+        
         ; light field
         (if (= off 0)
             (bufset-u8 tx-frame 8 light)
