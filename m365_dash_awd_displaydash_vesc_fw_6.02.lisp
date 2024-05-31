7b0146bdc3832f4a3cdada6038c3d88692b293e3
PierreDisplayDash <contact@displaydash.eu>
2024-05-31 16:03:45 +0200

    Update m365_dash_awd_displaydash_vesc_fw_6.02.lisp

--- m365_dash_awd_displaydash_vesc_fw_6.02.lisp
+++ m365_dash_awd_displaydash_vesc_fw_6.02.lisp
@@ -1,4 +1,4 @@
-; test 9
+; test 10
 ; M365 dashboard compability lisp script v0.6 by Netzpfuscher and 1zuna
 ; UART Wiring: red=5V black=GND yellow=COM-TX (UART-HDX) green=COM-RX (button)+3.3V with 1K Resistor
 ; Guide (German): https://rollerplausch.com/threads/vesc-controller-einbau-1s-pro2-g30.6032/
@@ -196,9 +196,9 @@
         )
         
         ; error field
-        (if (< count 10)
+        (if (< count 100)
             (progn
-                (bufset-u8 tx-frame 11 88)
+                (bufset-u8 tx-frame 11 56)
                 (setvar 'count (+ count 1)))
             (bufset-u8 tx-frame 11 (get-fault))
         )
