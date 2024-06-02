bf73fefa60f9fde0a90b8a2ca00b6dbb91a293f6
PierreDisplayDash <contact@displaydash.eu>
2024-06-02 10:53:27 +0200

    Update m365_dash_awd_displaydash_vesc_fw_6.02.lisp
    v14 fixed

--- m365_dash_awd_displaydash_vesc_fw_6.02.lisp
+++ m365_dash_awd_displaydash_vesc_fw_6.02.lisp
@@ -1,4 +1,4 @@
-; test 19
+; test 20
 ; M365 dashboard compability lisp script v0.6 by Netzpfuscher and 1zuna
 ; UART Wiring: red=5V black=GND yellow=COM-TX (UART-HDX) green=COM-RX (button)+3.3V with 1K Resistor
 ; Guide (German): https://rollerplausch.com/threads/vesc-controller-einbau-1s-pro2-g30.6032/
@@ -49,7 +49,7 @@
 (uart-start 115200 'half-duplex)
 (gpio-configure 'pin-rx 'pin-mode-in-pu)
 
-(define tx-frame (array-create 24))
+(define tx-frame (array-create 23))
 (bufset-u16 tx-frame 0 0x55AA)
 (bufset-u16 tx-frame 2 0x0821)
 (bufset-u16 tx-frame 4 0x6400)
@@ -227,7 +227,7 @@
         (bufset-u8 tx-frame 19 (get-temp-fet))
 
         ; temp mot field
-        (bufset-u8 tx-frame 20 (get-temp-mot))
+        (bufset-u8 tx-frame 20 (get-temp-fet))
 
         ; dist field
         (bufset-u16 tx-frame 21 (get-dist-abs))
