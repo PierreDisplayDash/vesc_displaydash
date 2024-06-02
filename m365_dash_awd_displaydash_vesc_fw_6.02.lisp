a78a6066a2f4003600c8ea83b51de10bb4c1337a
PierreDisplayDash <contact@displaydash.eu>
2024-06-02 10:31:03 +0200

    Update m365_dash_awd_displaydash_vesc_fw_6.02.lisp
    fix index and temp mot

--- m365_dash_awd_displaydash_vesc_fw_6.02.lisp
+++ m365_dash_awd_displaydash_vesc_fw_6.02.lisp
@@ -1,4 +1,4 @@
-; test 17
+; test 18
 ; M365 dashboard compability lisp script v0.6 by Netzpfuscher and 1zuna
 ; UART Wiring: red=5V black=GND yellow=COM-TX (UART-HDX) green=COM-RX (button)+3.3V with 1K Resistor
 ; Guide (German): https://rollerplausch.com/threads/vesc-controller-einbau-1s-pro2-g30.6032/
@@ -227,10 +227,10 @@
         (bufset-u8 tx-frame 19 (get-temp-fet))
 
         ; temp mot field
-        (bufset-u8 tx-frame 20 (get-temp-fet))
+        (bufset-u8 tx-frame 20 (get-temp-mot))
 
         ; dist field
-        (bufset-u16 tx-frame 20 (get-dist-abs))
+        (bufset-u16 tx-frame 21 (get-dist-abs))
         
         ; batt field
         (bufset-u8 tx-frame 23 (*(get-batt) 100))
